5a7896932ec49977fca91d067f4df8c540f4c867
Ryan Daily <ryan.daily.et@gmail.com>
2016-12-12 02:52:09 -0800

    Index.php update to account for payment stuff

--- index.php
+++ index.php
@@ -8,7 +8,14 @@ define("NS_BASE", "roommates\\hw5\\");
 
 define(NS_BASE . "NS_CONTROLLERS", "roommates\\hw5\\controllers\\");
 
-$controller_name = NS_CONTROLLERS . "LandingController";
-/* if ($something_or_other) {*/
-$lC = new $controller_name();
-$lC->processRequest();
+if(isSet($_POST['stripeToken'])){
+	$controller_class = "ChargeController";
+}
+else{
+	$controller_class = "LandingController";
+}
+
+$controller_name = NS_CONTROLLERS . $controller_class;
+$controller = new $controller_name();
+$controller->processRequest();
+
