8f1acdcd1a01d111cef796526ddbd34969fcd83e
David_Desktop <davidparker241@gmail.com>
2016-12-12 21:44:26 -0800

    Got the pdf to finally work

--- src/views/ChargeView.php
+++ src/views/ChargeView.php
@@ -2,13 +2,14 @@
 
 namespace roommates\hw5\views;
 require_once 'vendor/autoload.php';
-
 use roommates\hw5\resources as A;
 
 /**
 * This class renders the landing page to the website. 
 */
 class ChargeView implements view{
+	
+
 	/**
 	* This function spits out the html for the landing page of the website
 	*
@@ -48,6 +49,31 @@ class ChargeView implements view{
 				Payment successfully charged
 				<br>
 				<h1>Check out this cool PDF of your wish</h1>
+				<?php 
+					ob_start();
+					$height = 10;
+					$imageNumber = 1;
+					$source = 'src/resources/fountain_' . $imageNumber . '.jpg';
+					$wish = 'This is my stupid wish';
+					$coin = 'coin';
+					$fountain_name = 'fountain_name';
+					$pdf = new \FPDF();
+					$pdf->AddPage();
+					$pdf->SetFont('Arial','B',16);
+
+					$pdf->Cell(180,10,$wish,0,1,'C');
+					$pdf->Cell(180, 10, $coin,0,1,'C');
+					$pdf->Cell(180, 10, $fountain_name,0,1,'C');
+					$pdf->Image($source,20 ,50);
+
+
+					$pdf->Output();
+					ob_end_flush();
+
+
+
+
+				 ?>
 			</body>
 		</html>		
 		<?php 
